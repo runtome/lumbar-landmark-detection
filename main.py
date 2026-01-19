f9c41db86174e9a1b9d797308f3729d5516e2457
SUPHOT NARAPONG <suphot.n@gmail.com>
2026-01-19 23:50:50 +0700

    Add Main entry point

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+import yaml
+
+from train.train_vit import train_vit
+# from train.train_cnn import train_cnn
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True)
+    parser.add_argument("--model", type=str, choices=["vit", "cnn"], required=True)
+    args = parser.parse_args()
+
+    cfg = yaml.safe_load(open(args.config))
+
+    if args.model == "vit":
+        train_vit(cfg)
+    # elif args.model == "cnn":
+    #     train_cnn(cfg)
+
+if __name__ == "__main__":
+    main()
