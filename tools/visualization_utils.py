79bdd8c67a043d495b2cee5946f4f2087ceac23e
SUPHOT NARAPONG <suphot.n@gmail.com>
2026-01-23 09:50:08 +0700

    Debug

--- tools/visualization_utils.py
+++ tools/visualization_utils.py
@@ -3,11 +3,10 @@ import numpy as np
 import cv2
 import torch
 
-
 def draw_landmarks(
-    image,            # [1, H, W] tensor
-    gt=None,           # [N, 2] normalized
-    pred=None,         # [N, 2] normalized
+    image,            # torch [1,H,W]
+    gt=None,           # [N,2] normalized (torch or numpy)
+    pred=None,         # [N,2] normalized (torch or numpy)
     radius=4
 ):
     img = image.squeeze().cpu().numpy()
@@ -17,22 +16,56 @@ def draw_landmarks(
     H, W = img.shape[:2]
 
     def to_px(coords):
-        coords = coords.clone()
+        if torch.is_tensor(coords):
+            coords = coords.detach().cpu().clone().numpy()
+        else:
+            coords = coords.copy()
+
         coords[:, 0] *= W
         coords[:, 1] *= H
         return coords
 
     if gt is not None:
         gt = to_px(gt)
-        for (x, y) in gt:
+        for x, y in gt:
             cv2.circle(img, (int(x), int(y)), radius, (0, 255, 0), -1)
 
     if pred is not None:
         pred = to_px(pred)
-        for (x, y) in pred:
+        for x, y in pred:
             cv2.circle(img, (int(x), int(y)), radius, (255, 0, 0), -1)
 
     return img
+
+# def draw_landmarks(
+#     image,            # [1, H, W] tensor
+#     gt=None,           # [N, 2] normalized
+#     pred=None,         # [N, 2] normalized
+#     radius=4
+# ):
+#     img = image.squeeze().cpu().numpy()
+#     img = (img * 255).astype(np.uint8)
+#     img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+#     H, W = img.shape[:2]
+
+#     def to_px(coords):
+#         coords = coords.clone()
+#         coords[:, 0] *= W
+#         coords[:, 1] *= H
+#         return coords
+
+#     if gt is not None:
+#         gt = to_px(gt)
+#         for (x, y) in gt:
+#             cv2.circle(img, (int(x), int(y)), radius, (0, 255, 0), -1)
+
+#     if pred is not None:
+#         pred = to_px(pred)
+#         for (x, y) in pred:
+#             cv2.circle(img, (int(x), int(y)), radius, (255, 0, 0), -1)
+
+#     return img
 
 
 LEVEL_ORDER = ["L1/L2", "L2/L3", "L3/L4", "L4/L5", "L5/S1"]
